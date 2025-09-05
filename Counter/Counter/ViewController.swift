4e32f508d3271799b3e5109c8fe1a7bd3fdd06fe
denmarin <Danya6846084@gmail.com>
2025-09-05 06:01:46 +0300

    added saves

--- Counter/Counter/ViewController.swift
+++ Counter/Counter/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var counter: UILabel!
     @IBOutlet weak var buttonPlus: UIButton!
     @IBOutlet weak var buttonMinus: UIButton!
@@ -16,23 +16,41 @@ class ViewController: UIViewController {
     @IBOutlet weak var historyTextView: UITextView!
     
     var amount = 0
+    let amountKey = "counter.amount"
+    let historyKey = "counter.history"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let ud = UserDefaults.standard
+        if ud.object(forKey: amountKey) != nil {
+            amount = ud.integer(forKey: amountKey)
+        } else {
+            amount = 0
+        }
+        let savedHistory = ud.string(forKey: historyKey) ?? "История изменений:\n"
+        
         counter.text = "Значение счётчика: \(amount)"
-        historyTextView.text = "История изменений:\n"
+        historyTextView.text = savedHistory
     }
     
     func currentDateString() -> String {
-            let formatter = DateFormatter()
-            formatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
-            return formatter.string(from: Date())
-        }
-
+        let f = DateFormatter()
+        f.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        return f.string(from: Date())
+    }
+    
+    func save() {
+        let ud = UserDefaults.standard
+        ud.set(amount, forKey: amountKey)
+        ud.set(historyTextView.text, forKey: historyKey)
+    }
+    
     @IBAction func plus1(_ sender: UIButton) {
         amount += 1
         counter.text = "Значение счётчика: \(amount)"
         historyTextView.text += "[\(currentDateString())]: значение изменено на +1\n"
+        save()
     }
     
     @IBAction func minus1(_ sender: UIButton) {
@@ -43,13 +61,13 @@ class ViewController: UIViewController {
         } else {
             historyTextView.text += "[\(currentDateString())]: попытка уменьшить значение счётчика ниже 0\n"
         }
+        save()
     }
     
     @IBAction func zeroAmount(_ sender: UIButton) {
         amount = 0
         counter.text = "Значение счётчика: \(amount)"
         historyTextView.text += "[\(currentDateString())]: значение сброшено\n"
+        save()
     }
-    
 }
-
